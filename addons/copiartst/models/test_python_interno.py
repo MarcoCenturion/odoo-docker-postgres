3a7aeffdcb5710425c937610db8c61f0dce664a8
MarcoCenturion <aulavirtualturismo@gmail.com>
2022-09-21 23:01:55 -0300

    ruta al copiar tst

--- addons/copiartst/models/test_python_interno.py
+++ addons/copiartst/models/test_python_interno.py
@@ -1,13 +1,13 @@
 import re
 import pyperclip as clip
-fee = input("definir fee en usd (oficial): ")
+fee = input("Definir fee en usd (oficial): ")
 tst_amadeus = clip.paste()
+retenc = [0]
 
-#print(tst_amadeus)
 
 cia=re.findall('BG CXR: (..) |CARRIER (..)', tst_amadeus)#[0]
 cambio=re.findall('1USD=(......)', tst_amadeus)
-route=re.findall('\n \w{3}\v \w{3} \w{2}\n\w|\n([ |X]\w{3} ..)', tst_amadeus)
+route=re.findall('\v\  \d{1}  \w{2}( \w{3}|\w{4})', tst_amadeus)
 orig=re.findall('\n (\w{3})\n', tst_amadeus)[0]
 fare_ars=re.findall('\nARS(\D{0,3}\d{1,6}.\d{2})', tst_amadeus)[0]
 bagage=re.findall('(0P|20|30|32|2B|PC|1P|2P|3P)\n', tst_amadeus)
@@ -17,37 +17,10 @@ date=re.findall(r'(?:[ |*][A-Z]{1}.)((?:\d{2}\D{3} ))', tst_amadeus)
 fare_usd=re.findall('USD(\D{0,6}\d{1,6}.\d{2}) ', tst_amadeus)[0]
 ttl=re.findall('\n(ARS|AR|ARS )(\d{1,7}.\d{2})', tst_amadeus)[-1][-1]
 
-
-cia=list(cia)
-#print(cia)
-for i in cia:
-     if i != '':
-          cia = i
-'''
-
-print("Cotización aéreo")
-print("--------------------------------------------------------------")
-print("Ruta completa",route)
-:print("Origen",orig)
-print("Tarifa en pesos ARS",fare_ars)
-print("Equipaje permitido ",bagage)
-print("Ultimo día para emitir",ltd)
-print("Retención Ganancias ARS",retenc)
-print("Tarifa orifinal USD",fare_usd)
-print("Cia aérea emisora ",cia)
-print("Tipo de cambio 1USD = ARS",cambio)
-print("--------------------------------------------------------------")
-print("Total con impuestos ARS ",float(ttl)+float(fee))
-print("--------------------------------------------------------------")
-'''
-#print("Cía Emisora ",cia,"\n Origen ",orig, "Ruta ", route,"\nUltimo día para emitir",ltd,"\nEquipaje ",bagage,"\nA recuperar en AFIP ARS ",retenc,"\nTotal c/tax ",float(ttl)+float(fee))
-
 total=float(ttl)+(float(fee)*float(str(cambio[0])))
-#total=float(ttl)+float(fee)
 
-print(f'---------------------------\n* Cía Emisora: {str(cia)}\n* Origen: {str(orig)}\n* Ruta: {str(route)}\n* Ultimo día para emitir: {str(ltd)}\n* Equipaje incluido: {str(bagage)}\n* Tipo de Cambio oficial {str(cambio)}\n* Anticipo ganancias a recuperar en AFIP ARS: {str(retenc)} por pasajero \n* Total con impuestos ARS: {str(total)}\n----------------------------') 
+# Caso tkt internacional
+texto =(f'INTERNACIONAL\n---------------------------\n* Cía Emisora: {str(cia)}\n* Origen: {str(orig)}\n* Ruta: {str(route)}\n* Ultimo día para emitir: {str(ltd)}\n* Equipaje incluido: {str(bagage)}\n* Tipo de Cambio oricial {str(cambio)}\n* Anticipo ganancias a recuperar en AFIP ARS: {str(retenc)} por pasajero \n* Total con impuestos ARS: {str(total)}\n----------------------------') 
 
-#print(f'{float(str(cambio[0]))}')
-
-
-clip.copy(f'---------------------------\n* Cía Emisora: {str(cia)}\n* Origen: {str(orig)}\n* Ruta: {str(route)}\n* Ultimo día para emitir: {str(ltd)}\n* Equipaje incluido: {str(bagage)}\n* Tipo de Cambio oficial {str(cambio)}\n* Anticipo ganancias a recuperar en AFIP ARS: {str(retenc)} por pasajero \n* Total con impuestos ARS: {str(total)}\n----------------------------') 
+print(texto)
+clip.copy(texto)
